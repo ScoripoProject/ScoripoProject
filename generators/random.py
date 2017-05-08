9b728f18b4919c6af7d033012dd26cf3110d7fb3
aaronlaw <aaronlaw@gmail.com>
2017-05-08 17:14:54 +0800

    Form a basic structure that using module of python.

--- generators/random.py
+++ generators/random.py
@@ -1,2 +1,13 @@
 """A lucky draw system based on pseudo-random number generator.
 """
+
+import random # Python's built-in random
+
+class RandomNumber:
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def get_random(self):
+	    return random.randint(self.start, self.end)
--- luckydraw_tester.py
+++ luckydraw_tester.py
@@ -1,2 +1,10 @@
 """Test case of LuckyDraw.
 """
+
+from generators import random #import generators.random
+
+
+# main()
+if __name__ == "__main__":
+    rn = random.RandomNumber(1, 78)
+    print(rn.get_random())
